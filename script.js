1ca77e525f1eefb7949d11bad424bae27fa2a2bf
Mona-Lee Braga <codewith.monalee@gmail.com>
2022-07-16 20:32:24 -0300

    ajustando arquivos

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
-    if(event.type === 'touchstart') event.preventDefault();
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-    const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', active);
-    event.currentTarget.setAttribute('aria-label', active ? 'Fechar Menu' : 'Abrir Menu');
+	if(event.type === 'touchstart') event.preventDefault();
+	const nav = document.getElementById('nav');
+	nav.classList.toggle('active');
+	const active = nav.classList.contains('active');
+	event.currentTarget.setAttribute('aria-expanded', active);
+	event.currentTarget.setAttribute('aria-label', active ? 'Fechar Menu' : 'Abrir Menu');
 }
 
 btnMobile.addEventListener('click', toggleMenu);
